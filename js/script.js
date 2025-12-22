cf0882e1cef190266c53604e339dc14a52616989
Jairo <jairo.carrasco@alu.ceacfp.es>
2025-12-22 20:20:26 +0100

    correccion de bug y nuevos iconos

--- js/script.js
+++ js/script.js
@@ -2,18 +2,21 @@
 // Texto de animación de script y función
 
 const texto = "console.log('Hello World'); print('Hello World') public class Main { public static void main(String[] args) { System.out.println(\"Hello World\"); } } #include <stdio.h>\nint main() { printf(\"Hello World\\n\"); return 0; } #include <iostream>\nusing namespace std;\nint main() { cout << \"Hello World\" << endl; return 0; } using System;\nclass Program { static void Main() { Console.WriteLine(\"Hello World\"); } } <?php echo 'Hello World'; ?> puts 'Hello World' package main\nimport \"fmt\"\nfunc main() { fmt.Println(\"Hello World\") } fn main() { println!(\"Hello World\"); } import Foundation\nprint(\"Hello World\") fun main() { println(\"Hello World\") } console.log('Hello World'); #!/bin/bash\necho \"Hello World\" <!DOCTYPE html>\n<html>\n<body>\n<h1>Hello World</h1>\n</body>\n</html>console.log('Hello World'); print('Hello World') public class Main { public static void main(String[] args) { System.out.println(\"Hello World\"); } } #include <stdio.h>\nint main() { printf(\"Hello World\\n\"); return 0; } #include <iostream>\nusing namespace std;\nint main() { cout << \"Hello World\" << endl; return 0; } using System;\nclass Program { static void Main() { Console.WriteLine(\"Hello World\"); } } <?php echo 'Hello World'; ?> puts 'Hello World' package main\nimport \"fmt\"\nfunc main() { fmt.Println(\"Hello World\") } fn main() { println!(\"Hello World\"); } import Foundation\nprint(\"Hello World\") fun main() { println(\"Hello World\") } console.log('Hello World'); #!/bin/bash\necho \"Hello World\" <!DOCTYPE html>\n<html>\n<body>\n<h1>Hello World</h1>\n</body>\n</html>";
-let i = 0;
+let i = 200;
 function animacionCodigoEscrito() {
     const codigo = document.getElementById("codigo")
+
+    if (i == 200) {
+        codigo.innerHTML = texto.slice(0, i);
+    }
 
     if (i < texto.length) {
         codigo.innerHTML += texto.charAt(i);
         i++;
-        setTimeout(animacionCodigoEscrito, 20);
+        setTimeout(animacionCodigoEscrito, 10);
     }
     if (i == texto.length) {
-        i = 0;
-        codigo.innerHTML = "";
+        i = 200;
 
 
     }
@@ -24,11 +27,11 @@ animacionCodigoEscrito();
 // Texto de animación de contactos y función
 
 const contactoGmail = "jairocarrasco97.dev";
-const contactoGithub = "jairoCarrasco97";
-const contactoWhatsapp = "+34 622 87 75 70";
 const contactoGmailId = "contactoGmail";
+const contactoGithub = "jairoCarrasco97";
 const contactoGithubId = "contactoGithub";
-const contactoWhatsappId = "contactoWhatsapp";
+const contactoLinkedin = "Linkedin";
+const contactoLinkedinId = "contactoLinkedin";
 const lenguajesTitulo = "Lenguajes";
 const lenguajesTituloId = "lenguajesTitulo";
 const proyectosTitulo = "Proyectos";
@@ -76,7 +79,7 @@ function animacionContactosBorrado(contactoDiv, id, j) {
 
 animacionContactosEscrito(contactoGmail, contactoGmailId, j1);
 animacionContactosEscrito(contactoGithub, contactoGithubId, j2);
-animacionContactosEscrito(contactoWhatsapp, contactoWhatsappId, j3);
+animacionContactosEscrito(contactoLinkedin, contactoLinkedinId, j3);
 animacionContactosEscrito(proyectosTitulo, proyectosTituloId, j4);
 animacionContactosEscrito(sobreMiTitulo, sobreMiTituloId, j5);
 animacionContactosEscrito(lenguajesTitulo, lenguajesTituloId, j6);
